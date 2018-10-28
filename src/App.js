31fbd265eff5bb2a8d07d21c24fe0c4128b91855
esle <essi.lehtola@hotmail.com>
2018-10-28 10:10:29 +0200

    just in case

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/Frontpage/NavBar';
 import test from './components/Frontpage/test';
 import Archived from './components/ArchivedPhotos/Archived';
+import MemberArea from './components/Memberarea/MemberArea';
 
 class App extends Component {
 
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/photo-today" component={TodaysPhoto}></Route>
             <Route path="/archived-photos" component={Archived}></Route>
             <Route path="/test" component={test}></Route>
+            <Route path="/member-area" component={MemberArea}></Route>
           </Switch>
         </div>
       </Router>
---
+++ src/components/Memberarea/MemberArea.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class MemberArea extends Component {
+    render() {
+        return (
+            <div>
+                <div className="Login">
+
+                </div>
+                <div className="Register">
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MemberArea;
+
+
+class LoginBox extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { };
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
